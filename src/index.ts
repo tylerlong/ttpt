f715f3a140acca6c4cccc735c783011391aaad5c
Tyler Long <tyler4long@gmail.com>
2023-03-09 11:00:08 -0800

    Disable no-undef

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,8 @@ module.exports = {
     'prettier/prettier': ['error'],
     // todo: https://github.com/AlloyTeam/eslint-config-alloy/issues/239
     '@typescript-eslint/no-unused-vars': ['error'],
+    // todo: https://github.com/AlloyTeam/eslint-config-alloy/issues/241
+    'no-undef': ['off'],
   },
 };
   `,
