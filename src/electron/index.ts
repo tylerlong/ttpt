3e3f508165797dd2f677b6cb2681e9c1d71f4597
Tyler Liu <tyler4long@gmail.com>
2023-04-01 16:47:41 -0700

    Update electron watch script

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -68,10 +68,13 @@ export default config;
 import { run } from 'shell-commands';
 
 const main = async () => {
-  await run('rm -rf build');
+  await run(\`
+    rm -rf .parcel-cache
+    rm -rf build
+  \`);
   run("nodemon --watch src/preload.ts --exec 'parcel build --target preload'");
   run('parcel src/index.html --dist-dir build -p 1234');
-  run('parcel watch --target electron -p 1235');
+  run('parcel watch --target electron -p 1240');
 };
 
 main();
