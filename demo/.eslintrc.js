a173f9feebc3687774195fbe990ae4775d7ef613
Tyler Liu <tyler4long@gmail.com>
2024-08-22 21:55:00 -0700

    Going to update r3f

--- demo/.eslintrc.js
+++ demo/.eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  extends: ['alloy', 'alloy/react', 'alloy/typescript', 'prettier'],
+  extends: ['alloy', 'alloy/react', 'alloy/typescript', 'plugin:@react-three/recommended', 'prettier'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error'],
     quotes: ['error', 'single', { avoidEscape: true }],
     'prefer-const': ['error'],
+    'react/no-unknown-property': ['off'], // https://github.com/jsx-eslint/eslint-plugin-react/issues/3423
   },
   settings: {
     react: {
--- demo/src/app.tsx
+++ demo/src/app.tsx
@@ -1,35 +1,33 @@
 import React from 'react';
-import { Button, Space, Typography } from 'antd';
+import { Canvas } from '@react-three/fiber';
 import { auto } from 'manate/react';
 
 import type { Store } from './store';
 
-const { Text, Title } = Typography;
-
 const App = (props: { store: Store }) => {
-  const { store } = props;
-  const render = () => (
-    <>
-      <Title>Untitled App</Title>
-      <Space>
-        <Button
-          onClick={() => {
-            store.count -= 1;
-          }}
-        >
-          -
-        </Button>
-        <Text>{store.count}</Text>
-        <Button
-          onClick={() => {
-            store.count += 1;
-          }}
-        >
-          +
-        </Button>
-      </Space>
-    </>
-  );
+  const render = () => {
+    return (
+      <Canvas camera={{ position: [0, 3, 8] }}>
+        {/* Lighting */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[0, 3, 5]} intensity={0.5} />
+
+        {/* Objects */}
+        <mesh position={[-3, 0, 0]}>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        <mesh position={[0, 0, 0]}>
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        <mesh position={[3, 0, 0]}>
+          <boxGeometry />
+          <meshStandardMaterial color="green" />
+        </mesh>
+      </Canvas>
+    );
+  };
   return auto(render, props);
 };
 
